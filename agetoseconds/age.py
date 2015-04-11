c3337c8faf609403090e8ebdf2fe7f3120245938
shinma <shinma@Manticore.com>
2015-04-11 22:11:20 +0200

    Renamed base to unit. Refactored every occurence in the source. Refactored the init interface. Added a mapping for unit string literals to unit integers, these integers are internally used by the conversion algorithm Added some docstrings.

--- agetoseconds/age.py
+++ agetoseconds/age.py
@@ -7,81 +7,111 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class Base:
+class Unit:
     seconds, minutes, hours, days, weeks, years = range(6)
 
 
 class Age(object):
-    def __init__(self, year, month, day, hour=0, minute=0, second=0, **kwargs):
-        self._dateObject = datetime.date(year, month, day)
-        if kwargs.get('dateObject'):
-            self._dateObject = kwargs.get('dateObject')
-        if kwargs.get('timeObject'):
-            self._timeObject = kwargs.get('timeObject')
-        else:
-            self._timeObject = datetime.time(hour, minute, second, 0)
+    """Represents any age by wrapping around a python datetime.
+    """
+    
+    def __init__(self, date_object, time_object=None, **kwargs):
+        self._dateObject = date_object
+        if not time_object:
+            time_object = datetime.time()
+        self._timeObject = time_object
         self._age = datetime.datetime.combine(self._dateObject, self._timeObject)
         self._conversion = [0]*6
-        self._base = Base.seconds
+        self._unit = Unit.seconds
     
-    
+    # map unit literals to a unit enumeration value
+    units = {
+            'seconds': Unit.seconds,
+            'minutes': Unit.minutes,
+            'hours': Unit.hours,
+            'days': Unit.days,
+            'weeks': Unit.weeks,
+            'years': Unit.years,
+            }
+
+
     @property
-    def base(self):
-        return self._base
-    
-    
-    @base.setter
-    def base(self, unit):
-        assert unit >= Base.seconds
-        assert unit <= Base.years
-        self._base = unit
-    
-    
+    def unit(self):
+        return self._unit
+
+
+    @unit.setter
+    def unit(self, unit):
+        assert unit in Age.Units
+        #assert unit >= Unit.seconds
+        #assert unit <= Unit.years
+        self._unit = Age.Units.get(unit)
+
+
     def get(self):
-        ''' This method returns your age as an integer by default.
-        If 'base' was set to another unit it will return a tuple  with 
-        the first element being the value 'seconds' up to the specified 
-        base unit.
+        '''This method returns your age as an integer or tuple value
+        
+        By default, this method will return an integer counting the
+        age in seconds.
+        
+        When the 'unit' property was set to a unit of a higher rank,
+        this method returns a tuple.
+        The first element in the tuple is the unit specified. Each 
+        consecutive element is the value of the next lower ranked unit,
+        down to seconds.
         '''
         self._conversion = [ 0 for val in self._conversion ]
-        self._conversion[Base.seconds] = self._calculate()
-        report = self._conversion[Base.seconds]
-        if self._base > Base.seconds:
+        self._conversion[Unit.seconds] = self._calculate()
+        report = self._conversion[Unit.seconds]
+        if self._unit > Unit.seconds:
             self._convert()
-            report = tuple(self._conversion[:self._base+1])
+            report = tuple(self._conversion[:self._unit+1])
         return report
-    
-    
-    def setTime(self, hour=0, minute=0, second=0, **kwargs):
-        if kwargs.get('timeObject'):
-            self._timeObject = kwargs.get('timeObject')
-        else:
-            self._timeObject = datetime.time(hour, minute, second, 0)
+
+
+    def setTime(self, time_object=None, **kwargs):
+        """Sets the internal time object after object creation.
+        
+        Keyword arguments precede the positional argument.
+        
+        Keyword arguments:
+          hour:   Positive integer
+          minute: Positive integer
+          second: Positive integer
+        """
+        if kwargs:
+            arguments = [ kwargs.get(k) if k in kwargs else 0 for k in ('hour','minute','second') ]
+            time_object = datetime.time(*arguments)
+        if not time_object:
+            time_object = datetime.time()
+        self._timeObject = time_object
         self._age = datetime.datetime.combine(self._dateObject, self._timeObject)
-    
-    
+
+
     def _calculate(self):
         now = datetime.datetime.now()
         timedelta = now - self._age
         result = int(timedelta.total_seconds())
-        log.debug("calculated timedelta -> {} seconds ".format(self._conversion[Base.seconds]))
+        log.debug("calculated timedelta -> {} seconds ".format(self._conversion[Unit.seconds]))
         return result
+
     
-    
+    # Divisors are used in the conversion algorithm and can be indexed
+    # with unit enumeration values
     conversionDivisors = {
-        Base.seconds + 1 : 60,
-        Base.minutes + 1 : 60,
-        Base.hours + 1   : 24,
-        Base.days + 1    : 7,
-        Base.weeks + 1   : 52
-    }
-    
-    
+        Unit.seconds+1: 60,
+        Unit.minutes+1: 60,
+        Unit.hours+1: 24,
+        Unit.days+1: 7,
+        Unit.weeks+1: 52
+        }
+
+
     def _convert(self):
-        self._conversion = [ 0 if n > Base.seconds else val for n, val in enumerate(self._conversion)  ]
-        log.debug("converting to base {}".format(self._base))
-        for i in range(self._base):
-            if i == Base.years:
+        self._conversion = [ 0 if n > Unit.seconds else val for n, val in enumerate(self._conversion)  ]
+        log.debug("converting to unit {}".format(self._unit))
+        for i in range(self._unit):
+            if i == Unit.years:
                 break
             c_div = Age.conversionDivisors.get(i+1)
             total = self._conversion[i]
@@ -89,23 +119,22 @@ class Age(object):
             base_val = (total - remainder) / c_div
             self._conversion[i] = remainder
             self._conversion[i+1] = base_val
-            
             log.debug("iteration = {}".format(i))
             log.debug(" get 'total' from index {0} -> {1}".format(i, total))
             log.debug(" mod {0} division : value 'remaining' -> {1}".format(c_div, remainder))
-            log.debug(" integer division by {0} : value 'base' -> {1}".format(c_div, base_val))
+            log.debug(" integer division by {0} : value 'unit' -> {1}".format(c_div, base_val))
             log.debug(" index: {0}  {1}  ...".format(' '*(len(str(remainder))-1) + str(i), ' '*(len(str(base_val))-1) + str(i+1)))
             log.debug(" array: {0}, {1}, ...".format(remainder, base_val))
 
 
 if __name__ == "__main__":
-    
+
     import sys
     try:
         import logging
     except:
         raise
-    
+
     loglevels = {
         'debug'     : logging.DEBUG,
         'info'      : logging.INFO,
@@ -113,40 +142,8 @@ if __name__ == "__main__":
         'error'     : logging.ERROR,
         'critical'  : logging.CRITICAL,
     }
-    
+
     if len(sys.argv) > 1:
         loglevel = loglevels.get(sys.argv[1], logging.NOTSET)
         logging.basicConfig(level=loglevel)
     log = logging.getLogger(__name__)
-    
-    
-    bday = Age(1986, 8, 18)
-    log.info("age: {}".format(bday.get()))
-    bday.base = Base.years
-    log.info("base years: {}".format(bday.get()))
-    bday.base = Base.weeks
-    log.info("base weeks: {}".format(bday.get()))
-    bday.base = Base.days
-    log.info("base days: {}".format(bday.get()))
-    bday.base = Base.hours
-    log.info("base hours: {}".format(bday.get()))
-    bday.base = Base.minutes
-    log.info("base minutes: {}".format(bday.get()))
-    bday.base = Base.seconds
-    log.info("base seconds: {}".format(bday.get()))
-    
-    
-    bday.setTime(16, 45, 55)
-    log.info(bday.get())
-    bday.base = Base.years
-    log.info("base years: {}".format(bday.get()))
-    bday.base = Base.weeks
-    log.info("base weeks: {}".format(bday.get()))
-    bday.base = Base.days
-    log.info("base days: {}".format(bday.get()))
-    bday.base = Base.hours
-    log.info("base hours: {}".format(bday.get()))
-    bday.base = Base.minutes
-    log.info("base minutes: {}".format(bday.get()))
-    bday.base = Base.seconds
-    log.info("base seconds: {}".format(bday.get()))
